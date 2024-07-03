0a491db1796018320fe34e6a971d40082f080826
Jayden Sim <39719057+jaydensim@users.noreply.github.com>
2024-07-03 00:26:07 +0000

    freaky

---
+++ src/_routes/sydney-trains/t69-freakshore/freaktral-freakwood.railroute.ts
@@ -0,0 +1,26 @@
+const railroute = {
+  id: 'sydneytrains:t69-freaktral-freakwood',
+  name: 'Sydney Trains - T69 Freak Shore',
+  description: 'Freaktral to Freakwood via St Freakards',
+  headsign: ['Freakwood', 'Via St Freakards'],
+  line: {
+    name: 'Sydney Trains - T69',
+    short: 'T69',
+    colour: '#F99D1C',
+    textcolour: '#FFFFFF'
+  },
+  stations: [
+    'Freaktral',
+    'Freak Hall',
+    'Freakyard',
+    'Freaky Point',
+    'Freak Sydney',
+    'Freakerton',
+    'Freakstonecraft',
+    'St Freakards',
+    'Freaktarmon',
+    'Freakwood'
+  ]
+}
+
+export default railroute
